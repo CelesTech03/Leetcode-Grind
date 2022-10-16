b8f0849c475a7dde22ed28c1ff3d8c269374d336
Celi <57969388+CelesTech03@users.noreply.github.com>
2022-10-15 22:34:02 -0400

    Time: 387 ms (32.97%), Space: 15.5 MB (34.38%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,19 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        
+        d = {}
+        
+        for num in nums:
+            if num not in d:
+                d[num] = 1
+            if d[num] > len(nums) // 2:
+                return num 
+            else:
+                d[num] += 1
+                
+        
+        
+        
+        
+            
+ 
