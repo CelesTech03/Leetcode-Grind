e9edcafd04458e2be4a7853038a76390533f1016
Celi <57969388+CelesTech03@users.noreply.github.com>
2022-11-12 15:25:24 -0500

    Time: 131 ms, Memory: 15 MB - LeetHub

---
+++ 0two-sum/0two-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        values = {}
+        
+        for index, item in enumerate(nums):
+            complement = target - item
+            if complement in values:
+                return [values[complement], index]
+            else:
+                values[item] = index
