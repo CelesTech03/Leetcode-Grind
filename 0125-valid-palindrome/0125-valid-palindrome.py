a9d46bf1b961ed10913c7aec95e42f8413969e31
Celi <57969388+CelesTech03@users.noreply.github.com>
2023-06-06 21:39:44 -0400

    Time: 63 ms (49.89%), Space: 16.9 MB (64.41%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         
-        left, right = 0, len(s) - 1
+        left = 0
+        right = len(s) - 1
         
         while left < right:
             while left < right and not s[left].isalnum():
@@ -10,7 +11,5 @@ class Solution:
                 right -= 1
             if s[left].lower() != s[right].lower():
                 return False
-            left += 1
-            right -= 1
+            left, right = left + 1 , right - 1
         return True
-                
